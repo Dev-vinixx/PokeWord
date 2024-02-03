a8233301d4bcf6952f06be32fbe5c14dd404cf6b
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-02-03 15:03:18 -0300

    made the basis of the pokedex layout and location of basic pokemon information.

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,25 @@
-
+import styles from '../src/Main.module.css'
 
 function Main() {
 
   return (
-    <>
+    <body className={styles.body}>
+      <main className={styles.main}>
+        <section className={styles.sectionPokemonImg}>
+          <div className={styles.containerPokemonAbout}>
+            <p>pikachu</p>
+            <div className={styles.divIdAndInsignia}>
+              <span>#160</span>
+              <img className={styles.iconsInsignia} src="../public/unnamed.png" alt="" />
+            </div>
+          </div>
+          <div>
 
-    <p>Hello Word</p>
-
-    </>
+          </div>
+        </section>
+        <section className={styles.sectionPokemonCharacteristics}></section>
+      </main>
+    </body>
   )
 
 }
